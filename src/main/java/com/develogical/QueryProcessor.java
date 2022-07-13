f8ff62c9a80f6f0802889646b5ccc8af31ed3529
Felix Schlicht <ge65quf@mytum.de>
2022-07-13 11:54:33 +0200

    jefjfj

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,5 +1,7 @@
 package com.develogical;
 
+import java.util.Locale;
+
 import static java.lang.Integer.parseInt;
 
 public class QueryProcessor {
@@ -10,26 +12,56 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if(query.toLowerCase().contains("largest")){
-            String[] s=query.split("largest:");
-            s=s[1].split(",");
-            for(String t:s){
-                t=t.trim();
+        if (query.toLowerCase().contains("largest")) {
+            String[] s = query.split("largest:");
+            s = s[1].split(",");
+            for (String t : s) {
+                t = t.trim();
             }
-            int max=Integer.MIN_VALUE;
-            for(String t:s){
-                max=Math.max(parseInt(t),max);
+            int max = Integer.MIN_VALUE;
+            for (String t : s) {
+                max = Math.max(parseInt(t), max);
             }
-            return max+"";
+            return max + "";
         }
-        if(query.toLowerCase().contains("plus")){
-            String[] s=query.split(" ");
-            return Integer.max(parseInt(s[s.length-1]), parseInt(s[s.length-2]))+"";
+        if (query.toLowerCase().contains("plus")) {
+            String[] s = query.split(" ");
+            return Integer.max(parseInt(s[s.length - 1]), parseInt(s[s.length - 2])) + "";
         }
-        if(query.toLowerCase().contains("multiplied")){
-            String[] s=query.split(" ");
-            return (Integer.parseInt(s[s.length-1])*Integer.parseInt(s[s.length-4]))+"";
+        if (query.toLowerCase().contains("multiplied")) {
+            String[] s = query.split(" ");
+            return (Integer.parseInt(s[s.length - 1]) * Integer.parseInt(s[s.length - 4])) + "";
+        }
+        if (query.toLowerCase().contains("primes:")) {
+            String[] s = query.split("primes:");
+            s = s[1].split(",");
+            for (String t : s) {
+                t = t.trim();
+            }
+            for (String t : s) {
+                if(isprime(Integer.parseInt(t)))return Integer.parseInt(t)+"";
+            }
+
+        }
+        if (query.toLowerCase().contains("banana")) {
+            return "yellow";
         }
         return "";
     }
+
+    boolean isprime(int num) {
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            // condition for nonprime number
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            return true;
+        else return false;
+    }
+}
 }
