0c002b276cf9cc5f5a4acde59a42f9163929f3f1
Felix Schlicht <ge65quf@mytum.de>
2022-07-13 11:29:20 +0200

    jefjfj

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -7,8 +7,13 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } if (query.toLowerCase().contains("name")) {
-            return "Felix";
+        }
+        if(query.toLowerCase().contains("plus")){
+            String[] s=query.split(" ");
+            return (Integer.parseInt(s[s.length-1])+Integer.parseInt(s[s.length-3]))+"";
+        }
+        if(query.toLowerCase().contains("largest")){
+
         }
         return "";
     }
